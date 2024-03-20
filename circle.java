4b37c16b285ae6f8aa15417667f1e3ff8ed598c7
LuaDeveloped <luadeveloped@gmail.com>
2024-03-20 14:29:36 -0400

    Added setters/getters

--- circle.java
+++ circle.java
@@ -1,3 +1,12 @@
 public class Circle{
-    public int radius;
+    private int radius;
+
+    public void setRadius(double nr) {
+        if(nr > 0) {
+            radius = nr;
+        }
+    }
+    public double getRadius() {
+        return radius;
+    }
 }
